be3978c1e2aa2950bc7d586cb4b3abf6322ec0f0
Robbin Ploeger <robbplo@gmail.com>
2026-03-11 14:36:39 +0100

    POC for retrieval with bm25

--- src/__main__.py
+++ src/__main__.py
@@ -1,21 +1,43 @@
 from pathlib import Path
 from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.retrievers import BM25Retriever
+
 def main():
-    loader = DirectoryLoader(
-        path="./data/vllm-0.10.1",
-        glob="**/[!.]*.py",
-    )
+
+    # Load documents
     glob = Path.glob(Path("./data/vllm-0.10.1"), "**/[!.]*.py")
-    print(len(list(glob)))
     documents: list[Document] = []
+    print("loading documents")
     for path in glob:
         content = ""
         with open(path) as file:
             content = file.read()
-        print(len(content))
         document = Document(page_content=content)
+        document.metadata["path"] = path
         documents.append(document)
+
+
+    # Split text
+    print("splitting documents")
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=200,
+    )
+    split_documents = splitter.split_documents(documents)
+
+    # Retrieval
+    retriever = BM25Retriever.from_documents(split_documents)
+    retriever.k = 5
+    question = "What is the default lora_int_id value when lora_request is None in vLLM's sequence?"
+    results = retriever.invoke(question)
+    # print(results[0])
+    print(len(documents))
+
+
+    # Generation
+
 
 
 
