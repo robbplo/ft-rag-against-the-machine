67e34e0abac13e3f74560fe92be973484497f4a4
Robbin Ploeger <robbplo@gmail.com>
2026-04-02 14:34:09 +0200

    Add document splitting

--- src/data/python_dataset.py
+++ src/data/python_dataset.py
@@ -1,16 +1,22 @@
 from src.data.dataset import Dataset
 from src.data.document import Document
-from pydantic import BaseModel, Field
 from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter, Language
 
 class PythonDataset(Dataset):
     def getDocuments(self) -> list[Document]:
         glob = Path.glob(self.path, "**/[!.]*.py")
+        splitter = RecursiveCharacterTextSplitter.from_language(Language.PYTHON, chunk_size=2000)
         documents: list[Document] = []
         for path in glob:
-            content = ""
             with open(path) as file:
                 content = file.read()
-            document = Document(path=path, content=content, start_index=0, end_index=len(content))
-            documents.append(document)
+            for chunk in splitter.create_documents([content]):
+                start = content.index(chunk.page_content)
+                documents.append(Document(
+                    path=path,
+                    content=chunk.page_content,
+                    start_index=start,
+                    end_index=start + len(chunk.page_content),
+                ))
         return documents
